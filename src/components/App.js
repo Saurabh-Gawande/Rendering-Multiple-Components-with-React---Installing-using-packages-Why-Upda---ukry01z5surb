4ac7cac4e8202abb888efa706b6803c069b49fe0
Saurabh <saurabhgawande2016@gmail.com>
2022-12-24 23:08:07 +0530

    Assignment Completed

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,12 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">Saurabh</h1>
+      <p data-ns-test="project-description">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum tempore earum impedit qui cumque iusto adipisci voluptas quaerat nostrum, modi recusandae dolor, perspiciatis sed neque vitae eos magni laboriosam facilis.
+      </p>
+    </div>
   )
 }
 
